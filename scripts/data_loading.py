785e410d13c5379f56a4378fc5b584e663c9e994
Noah Selman <nselman@uchicago.edu>
2020-04-30 09:35:05 -0500

    resolved unexpected duplicate issue

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -3,6 +3,8 @@ import pandas as pd
 import geopandas as gpd
 import json
 
+BUNDLED_CITIES = ["MW210", "MW315", "MW314", "MW107"]
+
 ### fancy way of setting working directory
 DATA_FOLDER = join(dirname(abspath(dirname(__file__))), "data")
 SHAPE_FILES = ["mwi_admbnda_adm2_nso_20181016.shp", "mwi_admbnda_adm3_nso_20181016.shp"]
@@ -17,48 +19,73 @@ def go():
 	"""
 
 	adm3_homes, adm3_to_adm2_dict, adm3_to_adm3_dict = create_relations()
-	CI = import_current_infections()
+	CI = import_current_infections(adm3_homes)
 	return CI, adm3_homes, adm3_to_adm2_dict, adm3_to_adm3_dict
 
 
-def load_inputs():
+def load_inputs(bundled=True):
 	"""
 	Loads input files from cleaned data folder
 	"""
 
 	CI = pd.read_json(join(INPUTS_FOLDER, "CI.json"))
-	TA_Districts = pd.read_json(join(INPUTS_FOLDER, "TA_Districts.json"))
 
-	with open(join(INPUTS_FOLDER, "TA_adj_Dist.json")) as json_file: 
-		TA_adj_Dist = json.load(json_file)
+	if bundled:
 
-	with open(join(INPUTS_FOLDER, "TA_adj_TA.json")) as json_file: 
-		TA_adj_TA = json.load(json_file)
+		TA_Districts = pd.read_json(join(INPUTS_FOLDER, "TA_Districts_Bundled.json"))
+
+		with open(join(INPUTS_FOLDER, "TA_adj_Dist_Bundled.json")) as json_file: 
+			TA_adj_Dist = json.load(json_file)
+
+		with open(join(INPUTS_FOLDER, "TA_adj_TA_Bundled.json")) as json_file: 
+			TA_adj_TA = json.load(json_file)
+
+		# TA_Districts.merge(CI, how='left', right_on='ADM2', )
+
+	else:
+
+		TA_Districts = pd.read_json(join(INPUTS_FOLDER, "TA_Districts.json"))
+
+		with open(join(INPUTS_FOLDER, "TA_adj_Dist.json")) as json_file: 
+			TA_adj_Dist = json.load(json_file)
+
+		with open(join(INPUTS_FOLDER, "TA_adj_TA.json")) as json_file: 
+			TA_adj_TA = json.load(json_file)		
 
 	return CI, TA_Districts, TA_adj_Dist, TA_adj_TA
 
 
 
-def populate_inputs_folders():
+def populate_inputs_folders(bundled=True):
 	"""
-
 	"""
 
 	print("preparing data...")
-	CI, adm3_homes, adm3_to_adm2_dict, adm3_to_adm3_dict = go()
+	adm3_homes, adm3_to_adm2_dict, adm3_to_adm3_dict = create_relations(bundled)
+
+	is_bund = "_bundled" if bundled else ""
+
+	CI = import_current_infections(adm3_homes)
 
 	print("outputting files...")
 	CI.to_json(join(INPUTS_FOLDER, "CI.json"))
-	adm3_homes.to_json(join(INPUTS_FOLDER, "TA_Districts.json"))
+	adm3_homes.to_json(join(INPUTS_FOLDER, "TA_Districts" + is_bund + ".json"))
+	# adm3_homes_bundled.to_json(join(INPUTS_FOLDER, "TA_Districts_Bundled.json"))
 
-	with open(join(INPUTS_FOLDER, "TA_adj_Dist.json"), 'w') as json_file:
+	with open(join(INPUTS_FOLDER, "TA_adj_Dist" + is_bund + ".json"), 'w') as json_file:
 		json.dump(adm3_to_adm2_dict, json_file)
 
-	with open(join(INPUTS_FOLDER, "TA_adj_TA.json"), 'w') as json_file:
+	# with open(join(INPUTS_FOLDER, "TA_adj_Dist_Bundled.json"), 'w') as json_file:
+	# 	json.dump(adm3_to_adm2_dict_bundled, json_file)
+
+	with open(join(INPUTS_FOLDER, "TA_adj_TA" + is_bund + ".json"), 'w') as json_file:
 		json.dump(adm3_to_adm3_dict, json_file)
+	# with open(join(INPUTS_FOLDER, "TA_adj_TA_Bundled.json"), 'w') as json_file:
+	# 	json.dump(adm3_to_adm3_dict_bundled, json_file)
 
 
-def create_relations():
+
+def create_relations(bundled=True):
 	"""
 	"""
 
@@ -69,35 +96,69 @@ def create_relations():
 
 	### adm3 file
 	adm3 = gpd.read_file(join(DATA_FOLDER, SHAPE_FILES[1]))
-	adm3 = adm3[["ADM3_PCODE", "ADM2_PCODE", "ADM2_EN", "ADM3_EN", "geometry"]].rename({"ADM3_PCODE": "ADM3", \
-		"ADM2_PCODE": "ADM2"}, axis=1)
+	adm3 = adm3[["ADM3_PCODE", "ADM2_PCODE", "ADM2_EN", "ADM3_EN", "geometry"]].\
+		rename({"ADM3_PCODE": "ADM3", "ADM2_PCODE": "ADM2"}, axis=1)
 	adm3["ADM2"] = adm3["ADM2"].str.strip()
 	adm3["ADM3"] = adm3["ADM3"].str.strip()
+	# adm3.drop_duplicates(ignore_index=True, inplace=True)
+
+	if bundled:
+		adm3.loc[adm3["ADM2"].isin(BUNDLED_CITIES), "ADM3"] = \
+			adm3.loc[adm3["ADM2"].isin(BUNDLED_CITIES), "ADM2"]
+		adm3.loc[adm3["ADM2"].isin(BUNDLED_CITIES), "ADM3_EN"] = \
+			adm3.loc[adm3["ADM2"].isin(BUNDLED_CITIES), "ADM2_EN"]
+
+	print(adm3)
 
 	adm3_homes = adm3[["ADM2", "ADM3", "ADM2_EN", "ADM3_EN"]]
-
+	adm3_homes.drop_duplicates(ignore_index=True, inplace=True)
+	# print(adm3_homes)
 	### connect adm3s to 2s
-	tmp = gpd.sjoin(adm3, adm2, how='left', op='intersects') ## spacial join
-	adm3_to_adm2 = tmp[tmp["ADM2_left"] != tmp["ADM2_right"]] ## filter out matches with home adm
+	tmp = gpd.sjoin(adm3, adm2, how='left', op='intersects')
+	adm3_to_adm2 = tmp[tmp["ADM2_left"] != tmp["ADM2_right"]] 
+	adm3_to_adm2.drop_duplicates(ignore_index=True, inplace=True)
 
-	adm3_to_adm2_dict = df_to_dict(adm3_to_adm2[["ADM3", "ADM2_right"]])
+	adm3_to_adm2_dict  = df_to_dict(adm3_to_adm2[["ADM3", "ADM2_right"]])
 
 	### connect adjacent adm3s
 	tmp = gpd.sjoin(adm3, adm3, how="left", op='intersects')
-	adm3_to_adm3 = tmp.loc[tmp["ADM3_left"] != tmp["ADM3_right"], ["ADM3_left", "ADM3_right"]]
+	adm3_to_adm3 = tmp.loc[tmp["ADM3_left"] != tmp["ADM3_right"],
+		["ADM3_left", "ADM3_right"]]
+	adm3_to_adm3.drop_duplicates(ignore_index=True, inplace=False)
 	adm3_to_adm3_dict = df_to_dict(adm3_to_adm3[["ADM3_left", "ADM3_right"]])
 
 	return adm3_homes, adm3_to_adm2_dict, adm3_to_adm3_dict
 
 
-def import_current_infections():
+def import_current_infections(adm3_homes):
+	"""
+	imports current infections file.  can handle CIs at ADM2 or ADM3 level
+	"""
 
 	tmp = pd.read_csv(join(DATA_FOLDER, CURRENT_INFECTIONS))
-	loc_var = "ADM3_PCODE" if "ADM3_PCODE" in tmp.columns else "ADM2_PCODE" 
-	CI = tmp[[loc_var, "Current Infections"]]
-	CI[loc_var] = CI[loc_var].str.strip() 	
 
-	return CI.set_index(loc_var)
+	# if "ADM3_PCODE" in tmp.columns:
+	# 	loc_var = "ADM3_PCODE"
+
+	# elif "ADM2_PCODE" in tmp.columns:
+	# 	loc_var = "ADM2_PCODE"
+	loc_var = "ADM3_PCODE" if "ADM3_PCODE" in tmp.columns else "ADM2_PCODE" 
+	# CI["ADM3_PCODE"] = CI["ADM3_PCODE"].str.strip() 	
+
+	if loc_var == "ADM2_PCODE":
+		tmp = adm3_homes.merge(tmp, how="left", 
+			left_on="ADM2", 
+			right_on="ADM2_PCODE")
+
+		tmp = tmp.rename({"ADM3": "ADM3_PCODE"}, axis=1)
+
+	tmp.loc[tmp['ADM2_PCODE'].isin(BUNDLED_CITIES), 'ADM3_PCODE'] = \
+		tmp.loc[tmp['ADM2_PCODE'].isin(BUNDLED_CITIES), 'ADM2_PCODE']
+
+
+	CI = tmp[["ADM3_PCODE", "Current Infections"]].drop_duplicates(ignore_index=True)
+
+	return CI.set_index("ADM3_PCODE")
 
 
 def df_to_dict(df):
@@ -107,9 +168,17 @@ def df_to_dict(df):
 	'''
 
 	d = {}
+	# d_bundled = {}
+
 
 	for k, v in df.itertuples(index=False, name=None):
 		d[k] = d.get(k, []) + [v]
+
+		# adm2 = adm3_homes.loc[adm3_homes["ADM3"] == k]["ADM2"].item()
+		# if adm2 in BUNDLED_CITIES:
+		# 	k = adm2
+
+		# d_bundled[k] = d_bundled.get(k, []) + [v]
 
 	return d
 
--- scripts/search_TAs.py
+++ scripts/search_TAs.py
@@ -10,38 +10,18 @@ def calc_scores(total_cs, CI, adm3_homes, multiplier_dict):
 		total_cs.loc[total_cs["Degree"] == k, "Multiplier"] = v
 
 	total_cs["Calculated Score"] = total_cs["Multiplier"] * total_cs["Current Infections"]
+	# print(calc_scores)
 
 	scores = total_cs.groupby("ADM3")["Calculated Score"].sum().reset_index()
 
 	scores = adm3_homes.merge(scores, how='left', left_on="ADM3", right_on="ADM3")
-	scores = scores.merge(CI, how="left", left_on="ADM2", right_index=True)
+	scores = scores.merge(CI, how="left", left_on="ADM3", right_index=True)
 	scores['Final Score'] = scores['Calculated Score']
 	scores.loc[scores['Current Infections'] > 0, 'Final Score'] = scores.loc[scores['Current Infections'] > 0, 'Current Infections']
 	# scores["Calculated Score"] = scores.apply(lambda row: check_for_CI(row, CI, adm3_homes), axis=1)
 	# scores.loc[scores["Current Infections"] > 0] = scores.loc[scores["Current Infections"], 
 
 	return scores
-
-
-def check_for_CI(row, CI, adm3_homes):
-	"""
-	Checks if there are > 0 current infections in home district.
-	Returns the number number of infections if True, or the calculated
-	score if False
-	Inputs:
-		row: row of pd.DataFrame with columns "ADM3" and "Score"
-		CI (pd.Series): index is District and value is number of current infections
-		adm3_homes (dict): keys are TAs and values are the associated District
-	"""
-	adm2 = adm3_homes.get(row["ADM3"], None)
-	if adm2:
-		num_infections = CI.loc[adm2].item()
-		if num_infections > 0:
-			return num_infections
-		else:
-			return row["Score"]
-	else:
-		return row["Score"]
 
 
 def get_connections(adm3_list, adm3_to_adm3, CI, degree):
@@ -59,13 +39,14 @@ def get_connections(adm3_list, adm3_to_adm3, CI, degree):
 	Returns pd.DataFrame that lists all connections and their degree
 	"""
 
-	total_cs = pd.DataFrame(columns=["ADM2", "Degree", "ADM3", "Current Infections"])
+	total_cs = pd.DataFrame(columns=["adj_ADM3", "Degree", "ADM3", "Current Infections"])
 
 	for adm3 in adm3_list:
 
 		c = find_connections(adm3, adm3_to_adm3, degree)
 		c_df = pd.DataFrame(c, columns=["adj_ADM3", "Degree"])
 		min_c = c_df.groupby("adj_ADM3").min()
+		# print("max degree connection: {}".format(min_c.max()))
 		min_c["ADM3"] = adm3
 
 		merged = min_c.merge(CI, how="left", left_index=True, right_index=True)
@@ -85,18 +66,19 @@ def find_connections(adm3, adm3_to_adm3, degree, iteration=1):
 		interation (int): tracks which degree is being calculated
 	'''
 
-
 	adm3_list = adm3_to_adm3.get(adm3, [])
+	connections = []
 
 	if iteration == degree:
-		return [(adj_adm3, iteration) for adj_adm3 in adm3_list]
+		connections += [(adj_adm3, iteration) for adj_adm3 in adm3_list if adm3 != adj_adm3]
 
 	else:
 
-		connections = []
 		for adj_adm3 in adm3_to_adm3.get(adm3, []):
+			if adm3 == adj_adm3:
+				continue
 			connections += [(adj_adm3, iteration)]
-			connections += find_connections(adm3, adm3_to_adm3, degree, iteration=iteration+1)
+			connections += find_connections(adj_adm3, adm3_to_adm3, degree, iteration=iteration+1)
 
-		return connections
+	return connections
 
